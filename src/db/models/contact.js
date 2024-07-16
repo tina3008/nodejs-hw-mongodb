7d468983c701b97d43a5cdf6e4e0998dcb2d1426
tina3008 <kicja_3@ukr.net>
2024-07-16 14:16:51 +0300

    1

---
+++ src/db/models/contact.js
@@ -0,0 +1,35 @@
+// import { Schema } from 'mongoose';
+import mongoose from 'mongoose';
+const contactsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true | false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+ const ContactsCollection = mongoose.model('contacts', contactsSchema);
+export default ContactsCollection ;
