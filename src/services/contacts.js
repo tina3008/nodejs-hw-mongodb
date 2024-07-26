e6b6381aedb27a6c0272e539ad4f5a4fea1197ec
tina3008 <kicja_3@ukr.net>
2024-07-26 20:24:03 +0300

    filter

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -8,19 +8,19 @@ const getAllContacts = async ({
   perPage = 10,
   sortOrder = SORT_ORDER.ASC,
   sortBy = '_id',
-  // filter = {},
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
   const contactsQuery = ContactsCollection.find();
 
-//     if (filter.isFavourite) {
-//       contactsQuery.where('isFavourite').equals(filter.isFavourite);
-//     }
-// if (filter.contactType) {
-//  contactsQuery.where('contactType').equals(filter.contactType);
-// }
+    if (filter.isFavourite) {
+      contactsQuery.where('isFavourite').equals(filter.isFavourite);
+    }
+if (filter.contactType) {
+ contactsQuery.where('contactType').equals(filter.contactType);
+}
 
 
   const [contactsCount, contacts] = await Promise.all([
